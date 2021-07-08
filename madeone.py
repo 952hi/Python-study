19b71c66853bf45b40fc5b58affb12f37e81a8dd
952hi <dpsel57@gmail.com>
2021-07-08 22:57:21 +0900

    first commit

---
+++ madeone.py
@@ -0,0 +1,11 @@
+x = int(input("숫자입력"))
+cal = 1
+
+x = x//5
+print(x)
+
+    
+
+
+
+print(cal)
---
+++ night.py
@@ -0,0 +1,21 @@
+input_data = input()
+row = int(input_data[1])
+col = int(ord(input_data[0]))- int(ord('a')) +1
+cal = 0
+
+moves = [(-1,-2),(-2,-1),(-2,1),(-1,2),(1,2),(2,1),(2,-1),(1,-2)]
+print(col)
+
+
+for move in moves:
+    nextR = row + move[0]
+    nextC = col + move[1]
+    print(nextC,nextR)
+    
+
+    if nextR>=1 and nextR <=8:
+        if nextC >=1 and nextC <=8:
+            cal = cal + 1
+        
+print(cal) 
+
